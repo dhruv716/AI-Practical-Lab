0b1a616b518a47d07f090648b2f1852a9a9e4934
dhruv716 <121818483+dhruv716@users.noreply.github.com>
2023-11-06 22:07:12 +0530

    Update A_star.py

--- A_star.py
+++ A_star.py
@@ -1,81 +1,73 @@
-import heapq
+from collections import deque
 
-# Define the 8-puzzle game state representation
-class PuzzleState:
-    def __init__(self, board, parent=None, move=""):
-        self.board = board
-        self.parent = parent
-        self.move = move
-        self.g = 0  # Cost from the initial state
-        self.h = 0  # Heuristic value (estimated cost to reach the goal state)
-    
-    def __lt__(self, other):
-        return (self.g + self.h) < (other.g + other.h)
+class Graph:
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
 
-    def __eq__(self, other):
-        return self.board == other.board
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
 
-    def is_goal(self, goal_state):
-        return self.board == goal_state
+    def h(self, n):
+        H = {
+            'A': 1,
+            'B': 1,
+            'C': 1,
+            'D': 1
+        }
+        return H[n]
 
-    def generate_successors(self):
-        successors = []
-        row, col = self.board.index(0) // 3, self.board.index(0) % 3
-        moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    def a_star_algorithm(self, start_node, stop_node):
+        open_list = set([start_node])
+        closed_list = set([])
+        g = {}
+        g[start_node] = 0
+        parents = {}
+        parents[start_node] = start_node
 
-        for dr, dc in moves:
-            new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < 3 and 0 <= new_col < 3:
-                new_board = self.board[:]
-                new_board[row * 3 + col], new_board[new_row * 3 + new_col] = new_board[new_row * 3 + new_col], new_board[row * 3 + col]
-                successors.append(PuzzleState(new_board, self, move=f"Move {new_board[row*3+col]} to {new_row*3+new_col}"))
-        
-        return successors
+        while len(open_list) > 0:
+            n = None
 
-# Define the A* search algorithm
-def astar(initial_state, goal_state):
-    open_list = [initial_state]
-    closed_set = set()
+            for v in open_list:
+                if n is None or (g[v] + self.h(v) < g[n] + self.h(n)):
+                    n = v
 
-    while open_list:
-        current_state = heapq.heappop(open_list)
-        closed_set.add(tuple(current_state.board))
+            if n is None:
+                print('Path does not exist!')
+                return None
 
-        if current_state.is_goal(goal_state):
-            path = []
-            while current_state.parent:
-                path.append(current_state.move)
-                current_state = current_state.parent
-            path.reverse()
-            return path
+            if n == stop_node:
+                reconst_path = []
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+                reconst_path.append(start_node)
+                reconst_path.reverse()
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
 
-        for successor in current_state.generate_successors():
-            if tuple(successor.board) not in closed_set:
-                successor.g = current_state.g + 1
-                successor.h = heuristic(successor.board, goal_state.board)
-                heapq.heappush(open_list, successor)
+            for (m, weight) in self.get_neighbors(n):
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+            open_list.remove(n)
+            closed_list.add(n)
 
-    return None  # No solution found
+        print('Path does not exist!')
+        return None
 
-# Define a simple manhattan distance heuristic
-def heuristic(board, goal_board):
-    distance = 0
-    for i in range(9):
-        if board[i] != goal_board[i]:
-            r1, c1 = i // 3, i % 3
-            r2, c2 = goal_board.index(board[i]) // 3, goal_board.index(board[i]) % 3
-            distance += abs(r1 - r2) + abs(c1 - c2)
-    return distance
+adjacency_list = {
+    'A': [('B', 1), ('C', 3), ('D', 7)],
+    'B': [('D', 5)],
+    'C': [('D', 12)]
+}
 
-# Example usage
-if __name__ == "__main":
-    initial_state = PuzzleState([1, 2, 3, 8, 0, 4, 7, 6, 5])
-    goal_state = PuzzleState([1, 2, 3, 8, 0, 4, 7, 6, 5])
-
-    path = astar(initial_state, goal_state)
-    if path:
-        print("Solution found! Steps:")
-        for step in path:
-            print(step)
-    else:
-        print("No solution found.")
+graph1 = Graph(adjacency_list)
+graph1.a_star_algorithm('A', 'D')
